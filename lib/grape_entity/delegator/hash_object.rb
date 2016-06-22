c4c19bcff8d8e450bd26f92abeafd5b808b533f9
Nikita Bulaj <bulaj.nikita@gmail.com>
2016-06-22 17:29:31 +0300

    Fix #223 : extracting of string keys for Hashes
    Fix extracting of string keys for Grape::Entity::Delegator::HashObject.

--- lib/grape_entity/delegator/hash_object.rb
+++ lib/grape_entity/delegator/hash_object.rb
@@ -3,7 +3,7 @@ module Grape
     module Delegator
       class HashObject < Base
         def delegate(attribute)
-          object[attribute]
+          object[attribute] || object[attribute.to_s]
         end
       end
     end
