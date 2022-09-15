101f02c1a1721b0ca327b8d0d864c10ec6cd3e74
Daniel Dao <daniel.dao@chime.com>
2022-09-15 12:31:49 -0700

    🔥  remove `FetchableObject`

--- lib/grape_entity/delegator.rb
+++ lib/grape_entity/delegator.rb
@@ -15,8 +15,6 @@ module Grape
             HashObject
           elsif defined?(OpenStruct) && object.is_a?(OpenStruct)
             OpenStructObject
-          elsif object.is_a?(Hash) && object.respond_to?(:fetch, true)
-            FetchableObject
           else
             PlainObject
           end
